a3a7935fd7f5bf398c6e695012185e0d36cecb64
AI_Programming <118508502+IA-Programming@users.noreply.github.com>
2023-10-31 23:27:58 -0300

    Update streamlit_app.py
    did not work the cache_data method

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ with st.echo():
     
     driver = get_driver()
     
-    @st.cache_data
+    @st.cache_resource
     if url := st.input_text(label="put the url that you want you extract the html code", value="http://example.com", max_chars=100, help="pruepa"):
     
         start_time = time()
