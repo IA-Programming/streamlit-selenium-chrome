47af5c8202fa46fa859ddff3cb7e88a75208e9f6
AI_Programming <118508502+IA-Programming@users.noreply.github.com>
2023-11-15 21:06:56 -0300

    Update streamlit_app.py
    debugging 7

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,24 +16,22 @@ with st.echo():
     from selenium import webdriver
     from selenium.webdriver.support.wait import WebDriverWait
     from selenium.webdriver.chrome.options import Options
-    from selenium_driver import Driver
 
-    options = Options()
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36")
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
-    options.add_argument("--no-sandbox")
-    options.add_experimental_option("detach", True)
-    
     @st.cache_resource
     def get_driver():
+        options = Options()
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36")
+        options.add_argument('--disable-gpu')
+        options.add_argument('--headless')
+        options.add_argument("--no-sandbox")
+        options.add_experimental_option("detach", True)
         start_time = time()
         driver = webdriver.Chrome(options=options)
         elapsed_time = time() - start_time
         st.write(f"Chrome-driver loaded in {elapsed_time:.2f} seconds")
         return driver
     
-    driver = Driver()
+    driver = get_driver()
 
     from bs4 import BeautifulSoup
     from selenium.webdriver.support import expected_conditions as EC
@@ -48,39 +46,58 @@ with st.echo():
             print('\33[1;32m' + result + '\33[0m')
             return result, response.text
         else:
-
-            result = 'Using Selenium Script method'
-            print('\33[1;34m' + result + '\33[0m')
-
+            result= 'Using Selenium'
+            print('\33[1;33m' + result + '\33[0m')
+    
+            # Cargar la página web
             driver.get(Url)
-
+    
             # Esperar a que la página se cargue completamente
             WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.TAG_NAME, "body"))
             )  # Esperar hasta x segundos
-
-            # Get the HTML content directly from the browser's DOM
-            html_code = driver.execute_script("return document.body.outerHTML;")
-
-            html_code = BeautifulSoup(html_code, 'html.parser')
-
+    
+            html_code = driver.page_source
+    
             # Get the status code using requests library
             response = requests.get(driver.current_url)
-
-            # Validate the status code
             if response.status_code == 200:
                 result= result + ": " + "HTML code extracted successfully"
                 print(result)
                 return result, html_code
             else:
-                result= result + ": " + "Failed to extract HTML code"
-                print(result)
-                return result, html_code
+    
+                result = 'Using Selenium Script method'
+                print('\33[1;34m' + result + '\33[0m')
+                
+                # Get the HTML content directly from the browser's DOM
+                html_code = driver.execute_script("return document.body.outerHTML;")
+    
+                # Get the status code using requests library
+                response = requests.get(driver.current_url)
+    
+                # Validate the status code
+                if response.status_code == 200:
+                    result= result + ": " + "HTML code extracted successfully"
+                    print(result)
+                    return result, html_code
+                else:
+                    result= result + ": " + "Failed to extract HTML code"
+                    print(result)
+                    return result, html_code
     
     if url := st.text_input(label="put the url that you want you extract the html code", value="http://example.com", max_chars=100, help="pruepa"):
     
         start_time = time()
         result, html = ExtrayendoHTML(Url=url)
+        # Get the Chromium version
+        version = driver.capabilities['browserVersion']
+        # Print the version
+        st.markdown(f"## Chromium Version: {version}")
+        # Get the ChromeDriver version
+        chrome_driver_version = driver.capabilities['chrome']['chromedriverVersion'].split()[0]
+        # Print the ChromeDriver version
+        print(f"## ChromeDriver Version: {chrome_driver_version}")
         st.markdown(result)
         st.code(html)
         elapsed_time = time() - start_time
